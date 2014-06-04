a1439b0c194b2d22e8e0e2de3b4b4d8c748a2885
Hack Reactor Students <loqi@cruzio.com>
2014-06-04 16:23:55 -0700

    functional & functiona-shared stack implementation.

--- sprint-one/src/functional-shared/stack.js
+++ sprint-one/src/functional-shared/stack.js
@@ -1,8 +1,42 @@
 var makeStack = function() {
-  // Hey! Rewrite in the new style. Your code will wind up looking very similar,
-  // but try not not reference your old code in writing the new style.
+
+  var stack = {};
+
+  stack.storage = {};
+  stack.len = 0;
+
+  extend(stack, stackMethods);
+
+  return stack;
+};
+
+var extend = function(to, from){
+  for (var key in from){
+    to[key] = from[key];
+  }
 };
 
 var stackMethods = {};
+
+
+// size, pop, push
+stackMethods.size = function() {
+  return this.len;
+};
+
+stackMethods.push = function(value) {
+  this.storage[this.len] = value;
+  this.len++;
+};
+
+stackMethods.pop = function() {
+  if (this.len === 0) {
+    return null;
+  }
+  this.len--;
+  var ret = this.storage[this.len];
+  delete this.storage[this.len];
+  return ret;
+};
 
 
--- sprint-one/src/functional/stack.js
+++ sprint-one/src/functional/stack.js
@@ -3,15 +3,24 @@ var makeStack = function(){
 
   // Use an object with numeric keys to store values
   var storage = {};
+  var size = 0;
 
   // Implement the methods below
   someInstance.push = function(value){
+    storage[size] = value;
+    size++;
   };
 
   someInstance.pop = function(){
+    if (size===0) { return null; }
+    size--;
+    var ret = storage[size];
+    delete storage[size];
+    return ret;
   };
 
   someInstance.size = function(){
+    return size;
   };
 
   return someInstance;
