1bb7b61d995715342e715456bc3cd16223570adc
Hack Reactor Students <loqi@cruzio.com>
2014-06-05 16:10:20 -0700

    Prototypal steps completed

--- sprint-one/src/prototypal/queue.js
+++ sprint-one/src/prototypal/queue.js
@@ -1,8 +1,33 @@
 var makeQueue = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  var queue = Object.create(queueMethods);
+  return queue;
 };
 
-var queueMethods = {};
+var queueMethods = {
+  head    : 0,
+  tail    : 0,
+  storage : {}
+};
 
+queueMethods.enqueue = function(value){
+  this.storage[this.tail++] = value;
+};
 
+queueMethods.dequeue = function(){
+  if (this.size() === 0) {
+    this.head = 0;
+    this.tail = 0;
+    return null;
+  }
+  var ret = this.storage[this.head];
+  delete this.storage[this.head++];
+  return ret;
+};
+
+queueMethods.size = function(){
+  return this.tail - this.head;
+};
+
+// TypeError: Cannot convert undefined or null to object
--- sprint-one/src/prototypal/stack.js
+++ sprint-one/src/prototypal/stack.js
@@ -1,8 +1,32 @@
 var makeStack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+   var stack = Object.create(stackMethods);
+   return stack;
 };
 
-var stackMethods = {};
+var stackMethods = {
+  // push, pop, size
+  //
+};
 
+stackMethods._size = 0;
+stackMethods._storage = {};
 
+stackMethods.size = function() {
+  return this._size;
+};
+
+stackMethods.pop = function() {
+  if (this._size === 0) {
+    return null;
+  }
+  var ret = this._storage[--this._size];
+  delete this._storage[this._size];
+  return ret;
+};
+
+stackMethods.push = function(value) {
+  this._storage[this._size++] = value;
+
+};
