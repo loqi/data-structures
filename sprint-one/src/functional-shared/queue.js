52fc8cca57a58a27438c59cf04fc5f0cf6c7448e
Hack Reactor Students <loqi@cruzio.com>
2014-06-05 11:00:24 -0700

    Both queue and stack pass all tests for the two functional intantiation patterns.

--- sprint-one/src/functional-shared/queue.js
+++ sprint-one/src/functional-shared/queue.js
@@ -1,9 +1,34 @@
 var makeQueue = function(){
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  var queue = {};
+
+  queue.head = 0;
+  queue.tail = 0;
+  queue.storage = {};
+  extend(queue, queueMethods);
+  return queue;
+};
+
+var extend = function(to, from){
+  for (var key in from){
+    to[key] = from[key];
+  }
 };
 
 var queueMethods = {};
 
+queueMethods.enqueue = function(value){
+  this.storage[this.tail++] = value;
+};
 
+queueMethods.dequeue = function(){
+  if (this.size() < 1) {return null;}
+  var ret = this.storage[this.head];
+  delete this.storage[this.head++];
+  return ret;
+};
 
+queueMethods.size = function(){
+  return this.tail - this.head;
+};
--- sprint-one/src/functional/queue.js
+++ sprint-one/src/functional/queue.js
@@ -2,17 +2,31 @@ var makeQueue = function(){
   var someInstance = {};
 
   // Use an object with numeric keys to store values
-  var storage = {};
+  var _storage = {};
 
-  // Implement the methods below
+  var _tail = 0;
+  var _head = 0;
 
   someInstance.enqueue = function(value){
-  };
-
-  someInstance.dequeue = function(){
+    _storage[_tail] = value;
+    _tail++;
   };
 
   someInstance.size = function(){
+    return _tail - _head;
+  };
+
+
+  someInstance.dequeue = function(){
+    if (_tail-_head === 0){
+      _tail = 0;
+      _head = 0;
+      return null;
+    }
+    var ret = _storage[_head];
+    delete _storage[_head];
+    _head++;
+    return ret;
   };
 
   return someInstance;
